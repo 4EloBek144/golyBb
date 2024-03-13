e5873923a3d216d0d75dd1892d0f3a563b3ef955
Wihater <150428992+Wihater@users.noreply.github.com>
2024-03-13 20:53:53 +0700

    Сохранил просто
    Щас буду в коде ковыряться и что бы не всё сломать, сейвлю

---
+++ data/users.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from flask_login import *
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    works = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def check_password(self, password):
+        return password == self.hashed_password
---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import *
+from flask_wtf import *
+from flask_login import *
+from wtforms import EmailField, PasswordField
+from wtforms import BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from data import db_session
+from data.users import User
+from data.login_form import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+db_session.global_init("db/main.db")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return render_template('logout.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
