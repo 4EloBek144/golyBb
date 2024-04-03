8112421c24cc024a0ed7c03be0689007e29fe612
4EloBek144 <150430914+4EloBek144@users.noreply.github.com>
2024-04-03 18:50:23 +0700

    Delete data directory

--- data/form_index.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField
-from wtforms import SubmitField
-from wtforms.validators import DataRequired
-
-
-class IndexTForm(FlaskForm):
-    text = TextAreaField('Поиск', validators=[DataRequired()])
-    submit = SubmitField('Поиск')
--- data/seach_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, TextAreaField
-from wtforms import BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class SeachForm(FlaskForm):
-    text = TextAreaField('Поиск', validators=[DataRequired()])
-    submit = SubmitField('Искать')
